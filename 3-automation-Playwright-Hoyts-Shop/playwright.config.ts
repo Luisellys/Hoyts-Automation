937b9b74ae3322ee553eeee325050e9cf41e9348
Luisellys <luisellys.cedano@gmail.com>
2026-04-02 13:43:54 -0400

    update config file 2 workers fully parallel

--- 3-automation-Playwright-Hoyts-Shop/playwright.config.ts
+++ 3-automation-Playwright-Hoyts-Shop/playwright.config.ts
@@ -15,13 +15,13 @@ export default defineConfig({
   testDir: './tests',
   
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
     /* Top-level test timeout (applies to each test) */
