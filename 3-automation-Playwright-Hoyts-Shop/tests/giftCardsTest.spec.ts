a3d2e53493fdc494090900604bd88eeb46b1a141
Luisellys <Luisellys.cedano@gmail.com>
2026-04-23 18:00:34 -0400

    updated Json test data file

--- 3-automation-Playwright-Hoyts-Shop/tests/giftCardsTest.spec.ts
+++ 3-automation-Playwright-Hoyts-Shop/tests/giftCardsTest.spec.ts
@@ -1,23 +1,22 @@
 import {test, expect} from "../utils/baseTest";
 import { mainPageHoytsShop } from "../pages/pageObjects/mainPageHoytsShop";
+import testData from "../utils/test-data/giftCards.json"
 import { giftCardsPageHoytsShop } from "../pages/pageObjects/giftCardsPageHoytsShop";
 
 test('Pick Most Popular Gift Card', async({mainPage})=>{
-    const title = 'Michael';
-    const value = '150';
-    await mainPage.pickMostPopularGiftCard(title,value);
-    const chosenGiftCard = await mainPage.confirmGiftCard(title);
-    expect(chosenGiftCard?.trim()).toContain(title);
+    const data = testData.mostPopular[0]
+    await mainPage.pickMostPopularGiftCard(data.title, data.value);
+    const chosenGiftCard = await mainPage.confirmGiftCard(data.title);
+    expect(chosenGiftCard?.trim()).toContain(data.title);
 
 });
 
 test('Pick from Gift Cards Page', async({mainPage, page})=>{
-    const title = 'Super Mario';
-    const value = '80';
+    const data = testData.giftCardsPage[1]
     await mainPage.goToGiftCards();
     const giftCardPage = new giftCardsPageHoytsShop(page);
-    await giftCardPage.pickGiftCard(title, value);
-    const chosenGiftCard = await giftCardPage.confirmGiftCard(title);
-    expect(chosenGiftCard?.trim()).toContain(title);
+    await giftCardPage.pickGiftCard(data.title, data.value);
+    const chosenGiftCard = await giftCardPage.confirmGiftCard(data.title);
+    expect(chosenGiftCard?.trim()).toContain(data.title);
 
 });
